337100d72f0e1850e568e5166c5dd7557ca82534
EBolle <ernstbolle@gmail.com>
2021-01-17 13:19:08 +0100

    Added logger.py and setup the logging for etl.py

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -1,3 +1,4 @@
+from logger import logger
 import psycopg2
 from settings import DWH_DB_USER, DWH_DB_PASSWORD, DWH_ENDPOINT, DWH_PORT, DWH_DB
 from src.sql_queries import create_table_queries, drop_table_queries
@@ -16,14 +17,20 @@ def create_tables(cur, conn):
 
 
 def main():
-    conn = psycopg2.connect(f"postgresql://{DWH_DB_USER}:{DWH_DB_PASSWORD}@{DWH_ENDPOINT}:{DWH_PORT}/{DWH_DB}")
-    cur = conn.cursor()
+    try:
+        conn = psycopg2.connect(f"postgresql://{DWH_DB_USER}:{DWH_DB_PASSWORD}@{DWH_ENDPOINT}:{DWH_PORT}/{DWH_DB}")
+    except Exception as error:
+        logger.exception(f"ERROR while connecting to the Redshift cluster: {error}")
+    else:
+        cur = conn.cursor()
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+        drop_tables(cur, conn)
+        create_tables(cur, conn)
 
-    conn.close()
+        conn.close()
 
 
 if __name__ == "__main__":
+    logger.info("Creating the tables on the Redshift cluster...")
     main()
+    logger.info("The tables have been successfully created.")
--- scripts/etl.py
+++ scripts/etl.py
@@ -1,29 +1,48 @@
+from logger import logger
 import psycopg2
 from settings import DWH_DB_USER, DWH_DB_PASSWORD, DWH_ENDPOINT, DWH_PORT, DWH_DB
 from src.sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
-    for query in copy_table_queries:
-        cur.execute(query)
-        conn.commit()
+    logger.info("Loading the staging tables")
+    for idx, query in enumerate(copy_table_queries, start=1):
+        try:
+            cur.execute(query)
+        except Exception as error:
+            logger.exception(f"ERROR while loading staging table {idx}: {error}")
+        else:
+            conn.commit()
+            logger.info(f"Staging table {idx} / {len(copy_table_queries)} has been successfully loaded.")
 
 
 def insert_tables(cur, conn):
-    for query in insert_table_queries:
-        cur.execute(query)
-        conn.commit()
+    logger.info("Loading the analytical tables")
+    for idx, query in enumerate(insert_table_queries, start=1):
+        try:
+            cur.execute(query)
+        except Exception as error:
+            logger.exception(f"ERROR while loading analytical table {idx}: {error}")
+        else:
+            conn.commit()
+            logger.info(f"Analytical table {idx} / {len(insert_table_queries)} has been successfully loaded.")
 
 
 def main():
-    conn = psycopg2.connect(f"postgresql://{DWH_DB_USER}:{DWH_DB_PASSWORD}@{DWH_ENDPOINT}:{DWH_PORT}/{DWH_DB}")
-    cur = conn.cursor()
+    try:
+        conn = psycopg2.connect(f"postgresql://{DWH_DB_USER}:{DWH_DB_PASSWORD}@{DWH_ENDPOINT}:{DWH_PORT}/{DWH_DB}")
+    except Exception as error:
+        logger.exception(f"ERROR while connecting to the Redshift cluster: {error}")
+    else:
+        cur = conn.cursor()
     
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+        load_staging_tables(cur, conn)
+        insert_tables(cur, conn)
 
-    conn.close()
+        conn.close()
 
 
 if __name__ == "__main__":
+    logger.info("Loading the data into the Redshift cluster...")
     main()
+    logger.info("The data is successfully loaded.")
